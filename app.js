a5fa2fed1f93fc947da99cc22d831e4e0d1d20ca
scisaif <saiflll284@gmail.com>
2021-10-04 23:50:44 +0530

    final

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
+const navLogo = document.querySelector('#navbar__logo');
 
 // Display Mobile Menu
 const mobileMenu = () => {
@@ -8,3 +9,44 @@ const mobileMenu = () => {
 }
 
 menu.addEventListener('click', mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+    const elem = document.querySelector('.highlight');
+    const homeMenu = document.querySelector('#home-page');
+    const aboutMenu = document.querySelector('#about-page');
+    const servicesMenu = document.querySelector('#services-page');
+    let scrollPos = window.scrollY;
+
+    // adds 'highlight' class to my menu items
+    if(window.innerWidth > 960 && scrollPos < 600) {
+        homeMenu.classList.add('highlight');
+        aboutMenu.classList.remove('highlight');
+    }else if(window.innerWidth > 960 && scrollPos <1400){
+        homeMenu.classList.remove('highlight');
+        aboutMenu.classList.add('highlight');
+        servicesMenu.classList.remove('highlight');
+    }else if(window.innerWidth > 960 && scrollPos <2345){
+        aboutMenu.classList.remove('highlight');
+        servicesMenu.classList.add('highlight');
+    }else {
+        elem.classList.remove('highlight');
+    }
+
+}
+
+window.addEventListener('scroll', highlightMenu);
+window.addEventListener('click', highlightMenu);
+
+
+// Close mobile menu when a menu item is clicked
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active');
+    if(window.innerWidth < 960 && menuBars) {
+        menu.classList.toggle('is-active');
+        menuLinks.classList.remove('active');
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu);
+navLogo.addEventListener('click', hideMobileMenu);
